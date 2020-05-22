3dc577df0a89dca7b11b97db2e2fec757cd79f34
Sascha Sallès <sascha.salles@ynov.com>
2020-05-22 17:45:17 +0200

    Initial Commit

---
+++ Papers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Papers
+//
+//  Created by Sascha Sallès on 22/05/2020.
+//  Copyright © 2020 saschasalles. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
